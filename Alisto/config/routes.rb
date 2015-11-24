a1bab5fbd2ab0de9815a6b7f198a4418d18835ee
Crisostomo Ibarra <christov9999@gmail.com>
2015-11-24 13:15:03 +0800

    Created Static

--- Alisto/config/routes.rb
+++ Alisto/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/contactUs'
-  get 'welcome/index'
-  root 'welcome#index'
+  get 'static/index'
+  root 'static#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
