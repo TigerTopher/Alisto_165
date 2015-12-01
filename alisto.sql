2a364de042a75a81ad2750b0680b3a26eed70420
ivan.cedric10 <ivan.cedric10@gmail.com>
2015-12-01 23:38:46 +0800

    Added Area table

--- alisto.sql
+++ alisto.sql
@@ -1,6 +1,3 @@
-/* NOTE: Text file lang muna for documentation purposes.
-        85% yun, GC ako.
-*/
 
 CREATE TABLE Alisto.Users
 (
@@ -13,6 +10,7 @@ CREATE TABLE Alisto.Users
     contact_person  INT             null,
     reports_issued  INT             not null,
 	PRIMARY KEY (id),
+	INDEX fname (fname ASC),
 	INDEX lname (lname ASC)
 --   contact_no (multivalued. see table Alisto.UserContactNum)
 );
@@ -31,23 +29,33 @@ CREATE TABLE Alisto.UserContactNum
 (
     user_id         INT             PRIMARY KEY,
     num             VARCHAR(20)     PRIMARY KEY,
-	INDEX num (num ASC)
+);
+
+CREATE TABLE Alisto.Area
+(
+	id				INT				not null,
+	name			VARCHAR(20)		not null,
+	coordinate_x    DECIMAL         not null,
+    coordinate_y    DECIMAL         not null,
+	report_count	INT				not null,
+	PRIMARY KEY (id),
+	INDEX report_count (report_count DESC),
+	INDEX name (name ASC)
 );
 
 CREATE TABLE Alisto.Report
 (
     id              INT             not null,
+	reporter		INT				not null,
     title           VARCHAR(25)     not null,
-    num             INT             not null,
-    coordinate_x    DECIMAL         not null,
-    coordinate_y    DECIMAL         not null,
-	street			VARCHAR(20)		not null,
+	area			INT				not null,
     short_desc      TEXT            null,
     classification  INT             not null,
     date_issued     DATETIME        not null,
     full_report     LONGTEXT        null,
 	PRIMARY KEY (id),
-	INDEX street (street ASC)
+	INDEX date_issued (date_issued DESC),
+	INDEX area (area ASC)
 );
 
 CREATE TABLE Alisto.Classifications
@@ -55,7 +63,7 @@ CREATE TABLE Alisto.Classifications
     id              INT             not null,
     crime_name      VARCHAR(25)     not null,
     syndicate       INT             null,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
 );
 
 CREATE TABLE Alisto.Syndicate
@@ -63,6 +71,7 @@ CREATE TABLE Alisto.Syndicate
     id              INT             not null,
     name            VARCHAR(50)     not null,
     overview        LONGTEXT        null,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	INDEX name (name ASC)
 --   report_count  (derived)
 );
