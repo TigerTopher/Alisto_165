c4890312867c0cab55d8c2eee4148e8273f58046
Crisostomo Ibarra <christov9999@gmail.com>
2015-12-21 02:52:31 +0800

    Bago magreport

--- Alisto/app/helpers/application_helper.rb
+++ Alisto/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title='')
+		base_title = "Alisto - Liwanag sa UP Diliman"
+		if page_title.empty?
+			"Alisto | Liwanag sa UP Diliman"
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
