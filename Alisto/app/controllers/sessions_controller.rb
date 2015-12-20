090ed981ff2561370e297970eb7aeb73a080a6d1
Crisostomo Ibarra <christov9999@gmail.com>
2015-12-20 21:59:37 +0800

    pre 8.2.4 Testing Layout Changes

---
+++ Alisto/app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user 
+      redirect_to user #user_url(user)
+    else
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
--- Alisto/config/routes.rb
+++ Alisto/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'sessions/new'
+
   resources :users
   get 'static/index'
   root 'static#index'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
